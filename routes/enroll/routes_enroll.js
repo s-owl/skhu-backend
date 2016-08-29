c71859a36c56bbb4aaa6e15b190f7df099187aa4
Youngbin Han <sukso96100@gmail.com>
2016-08-29 14:40:23 +0000

    Revert "Revert "?""
    This reverts commit 63849a18697de50c50f52fc6492506c17bb876c2

--- routes/enroll/routes_enroll.js
+++ routes/enroll/routes_enroll.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 // POST - SUBJECTS
-// var subjects = require('./subjects');
-// router.post('/subjects', subjects);
+var subjects = require('./subjects');
+router.post('/subjects', subjects);
 
-
+var saved_credits = require('./saved_credits');
+router.post('/saved_credits', saved_credits);
 module.exports = router;
