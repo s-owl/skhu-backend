b7fae62f36690d990264aef6f7bdc7bcddedbca6
chugeonwoo <cnrjsdn@gmail.com>
2016-09-07 03:06:28 +0000

    Update serverinfo.js

--- routes/serverinfo.js
+++ routes/serverinfo.js
@@ -3,12 +3,14 @@ var run = function(req, res, next){
     Based on Node.js and Express.js <br> \
     Developed by :<br> \
     Youngbin Han (201632034 | sukso96100@gmail.com) from Sungkonghoe University - Software Engineering <br> \
-    Geonwoo Chu (201534028 | cnrjsdn@gmail.com)  from Sungkonghoe University - Software Engineering <br><br> \
+    Geonwoo Chu (201534028 | cnrjsdn@gmail.com) from Sungkonghoe University - Software Engineering <br> \
+    Daseul Bae (201232016 | dghdu@naver.com) from Sungkonghoe University - Software Engineering <br><br> \
     Foressst API 서버 <br> \
     Node.js 및 Express.js 기반으로 개발됨 <br> \
     개발자 : <br> \
     한영빈 (201632034 | sukso96100@gmail.com) 성공회대학교 소프트웨어공학과 <br> \
-    추건우 (201534028 | cnrjsdn@gmail.com) 성공회대학교 소프트웨어공학과";
+    추건우 (201534028 | cnrjsdn@gmail.com) 성공회대학교 소프트웨어공학과 <br> \
+    배다슬 (201232016 | dghdu@naver.com) 성공회대학교 소프트웨어공학과";
   res.send(info);
 }
   module.exports = run;
