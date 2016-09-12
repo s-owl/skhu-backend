b7839f1ebceff2daa50bb08a5c5d141d08871b92
sukso96100 <sukso96100@gmail.com>
2016-09-12 14:36:00 +0900

    학사일정 조회 추가

--- routes/page/routes_page.js
+++ routes/page/routes_page.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-// POST - YLLABUS_DETAILS
+// POST - SYLLABUS_DETAILS
 var syllabus_details = require('./syllabus_details');
 router.post('/syllabus_details', syllabus_details);
+
+// POST - CALENDAR
+var calendar = require('./calendar');
+router.post('/calendar', calendar);
 
 module.exports = router;
