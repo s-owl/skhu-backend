25a08fe88782fb574dcb0875a73411230722ec15
sukso96100 <sukso96100@gmail.com>
2016-10-23 21:05:15 +0900

    식단표 링크 추출 오류 수정

--- routes/life/meal.js
+++ routes/life/meal.js
@@ -1,21 +1,25 @@
 var utils = require('../utils');
-var defaultUrl = "http://skhu.ac.kr/uni_zelkova/uni_zelkova_4_3_first.aspx";
 // Show Meal URLs
 var getUrls = function(req, res, next){
   console.log("GET /life/meal/urls");
   console.log("REMOTE IP : " + req.ip);
   console.log("REMOTE IPS : " + req.ips);
-
+  var url = utils.skhuBaseUrl + "/uni_zelkova/uni_zelkova_4_3_list.aspx";
   utils.get(req, res, next, url, true)
   .then(function(window, rawData){
 
     var urls = [];
     window.$("table.board_list > tbody > tr")
       .each(function(index, element){
+        console.log(window.$( element ).children("td:eq(1)").html());
+        var rawtag = window.$( element ).children("td:eq(1)").html();
+        var splited = rawtag.split('href="')[1];
+        var itemurl = utils.skhuBaseUrl + "/uni_zelkova/" + splited.split('">')[0];
+        var finalurl = itemurl.replace("&amp;","&");
         urls.push({
-          "title" : window.$( element ).children("td:eq(1) > span > a").text(),
-          "url" : utils.skhuBaseUrl + window.$( element ).children("td:eq(1) > span > a").attr('href'),
-          "date" : window.$( element ).children("td:eq(3)").text(),
+          "title" : window.$( element ).children("td:eq(1)").text(),
+          "url" :  finalurl,
+          "date" : window.$( element ).children("td:eq(3)").text()
         })
       });
 
@@ -23,6 +27,7 @@ var getUrls = function(req, res, next){
       "urls" : urls
     }));
 });
+};
 exports.getUrls = getUrls;
 
 // Show Meal Data
@@ -32,7 +37,7 @@ var getData = function(req, res, next){
   console.log("REMOTE IPS : " + req.ips);
 
   // Parse Meal Data from the url
-  var defaultUrl = "http://skhu.ac.kr/uni_zelkova/uni_zelkova_4_3_first.aspx";
+  var defaultUrl = utils.skhuBaseUrl + "/uni_zelkova/uni_zelkova_4_3_first.aspx";
   var url = req.body.url == undefined || req.body.url == "" ? defaultUrl : req.body.url;
 
   utils.get(req, res, next, url, true)
@@ -48,21 +53,21 @@ var getData = function(req, res, next){
         "lunch" : {
           "a" : {
             "diet" : window.$(nav+" > tbody > tr:eq(0) > td:eq("+i+")").text(),
-            "calorie" : window.$(nav+" > tbody > tr:eq(1) > td:eq("+i+")").text(),
+            "calorie" : window.$(nav+" > tbody > tr:eq(1) > td:eq("+i+")").text()
           },
           "b" : {
             "diet" : window.$(nav+" > tbody > tr:eq(2) > td:eq("+i+")").text(),
-            "calorie" : window.$(nav+" > tbody > tr:eq(3) > td:eq("+i+")").text(),
+            "calorie" : window.$(nav+" > tbody > tr:eq(3) > td:eq("+i+")").text()
           },
           "c" : {
             "diet" : window.$(nav+" > tbody > tr:eq(4) > td:eq("+i+")").text(),
-            "calorie" : window.$(nav+" > tbody > tr:eq(5) > td:eq("+i+")").text(),
+            "calorie" : window.$(nav+" > tbody > tr:eq(5) > td:eq("+i+")").text()
           }
         },
         "dinner" : {
           "a" : {
             "diet" : window.$(nav+" > tbody > tr:eq(6) > td:eq("+i+")").text(),
-            "calorie" : window.$(nav+" > tbody > tr:eq(7) > td:eq("+i+")").text(),
+            "calorie" : window.$(nav+" > tbody > tr:eq(7) > td:eq("+i+")").text()
           }
         }
       });
@@ -73,5 +78,5 @@ var getData = function(req, res, next){
       "data" : meal
     }))
   });
-}
-module.getData = getData;
+};
+exports.getData = getData;
