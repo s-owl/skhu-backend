559b725f53f1e56132dcf7c345a7c599f15bf549
sukso96100 <sukso96100@gmail.com>
2016-12-01 21:27:26 +0900

    상담 이력 조회 추가

--- routes/life/routes_life.js
+++ routes/life/routes_life.js
@@ -1,20 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 
 // 학사일정 조회
 // POST - schedules
-var schedules = require('./schedules');
+const schedules = require('./schedules');
 router.post('/schedules', schedules);
 
 // 식단표 목록 조회
 // GET - meal/urls
-var meal_urls = require('./meal').getUrls;
+const meal_urls = require('./meal').getUrls;
 router.get('/meal/urls', meal_urls);
 
 // 식단표 데이터 조회
 // POST - meal/data
-var meal_data = require('./meal').getData;
+const meal_data = require('./meal').getData;
 router.post('/meal/data', meal_data);
+
+// 상담 이력 조회
+// POST　-　consulting
+const consulting = require('./consulting');
+router.post('/consulting', consulting);
 
 module.exports = router;
