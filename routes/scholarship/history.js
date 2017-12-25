11b00061dd7bf0a56242aa9347b828d5a36c9a9d
sukso96100 <sukso96100@gmail.com>
2017-12-25 16:30:16 +0900

    [feature/scholarship] 장학 내역 조회 및 장학 결과 조회 JSDOM 호출부 재작성

--- routes/scholarship/history.js
+++ routes/scholarship/history.js
@@ -1,40 +1,40 @@
-// cURL 유틸
-const curl_utils = require('../curl_utils');
-
+const utils = require('../utils');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 // 장학 신청 내역 조회
 const run = (req, res, next) => {
-  console.log("POST /scholarship/history");
-  console.log("REMOTE IP : " + req.ip);
-  console.log("REMOTE IPS : " + req.ips);
+  console.log("GET /scholarship/history");
 
   // 파일별 url 설정
-  const url = "http://forest.skhu.ac.kr/GATE/SAM/SCHOLARSHIP/S/SJHS01S.ASPX?&maincd=O&systemcd=S&seq=1";
-
-  // cURL.get() 호출
-  curl_utils.get(req, res, url).then((window) => {
-    // 장학 신청 내역 파싱
-    const history = [];
-    window.$("#dgList > tbody > tr")
-      .each( (index, element) => {
+  const url = `${utils.forestBaseUrl}/GATE/SAM/SCHOLARSHIP/S/SJHS01S.ASPX?&maincd=O&systemcd=S&seq=1`;
+  utils.get(req, res, url, true)
+    .then((rawData) => {
+      const { document } = (new JSDOM(rawData)).window;
+      // 장학 신청 내역 파싱
+      let history = [];
+      let items = document.querySelectorAll("table#dgList > tbody > tr");
+      for(let i=0; i<items.length; i++){
         history.push({
-          "year" : window.$( element ).children("td:eq(0)").text(),
-          "semester" : window.$( element ).children("td:eq(1)").text(),
-          "scholarship_name" : window.$( element ).children("td:eq(2)").text(),
-          "order" : window.$( element ).children("td:eq(3)").text(),
-          "grade" : window.$( element ).children("td:eq(4)").text(),
-          "entrance_scholarship" : window.$( element ).children("td:eq(5)").text(),
-          "registration_scholarship" : window.$( element ).children("td:eq(6)").text(),
-          "benefit" : window.$( element ).children("td:eq(7)").text(),
-          "note" : window.$( element ).children("td:eq(8)").text()
+          "year" : items[i].children[0].textContent,
+          "semester" : items[i].children[1].textContent,
+          "scholarship_name" : items[i].children[2].textContent,
+          "order" : items[i].children[3].textContent,
+          "grade" : items[i].children[4].textContent,
+          "entrance_scholarship" : items[i].children[5].textContent,
+          "registration_scholarship" : items[i].children[6].textContent,
+          "benefit" : items[i].children[7].textContent,
+          "note" : items[i].children[8].textContent
         });
-      });
+      }
 
-    // JSON 으로 처리하여 클라이언트에 응답
-    res.send(JSON.stringify({
-      "scholarship_history" : history
-    }));
-
-  }).catch((err) => { console.log(err) });
-}
+      // JSON 으로 처리하여 클라이언트에 응답
+      res.send(JSON.stringify({
+        "scholarship_history" : history
+      }));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = run;
