018880fb5bcec28f5b2de9c19b5f8de52b477428
sukso96100 <sukso96100@gmail.com>
2017-12-26 14:40:53 +0900

    [feature/swagger] Swagger 연동 작업. (#8)

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
 
-var routes = require('./routes/routes');
-
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -24,7 +23,7 @@ app.use('/', routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     console.log("REQ PATH : " + req.baseUrl);
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -47,6 +46,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.send("ERROR!\n" + err.message);
 });
-
 
 module.exports = app;
