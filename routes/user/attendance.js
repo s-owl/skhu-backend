68e39d6edea190ef762a594adf537c7e291dd0df
metamong12 <kj980926@naver.com>
2018-05-18 19:10:44 +0900

    attendance 수정

--- routes/user/attendance.js
+++ routes/user/attendance.js
@@ -1,7 +1,7 @@
 // cURL 유틸
 const curl_utils = require('../curl_utils');
 const utils = require('../utils');
-
+const {JSDOM} = require('jsdom');
 // 출결현황 조회
 const run = (req, res, next) => {
   console.log("POST /user/attendance");
@@ -12,30 +12,35 @@ const run = (req, res, next) => {
   const url = "http://forest.skhu.ac.kr/Gate/UniMainStudent.aspx";
 
   // cURL.get() 호출
-  curl_utils.get(req, res, url).then((window) => {
+  utils.get(req, res, url, true).then((rawData) => {
+    const { document } = (new JSDOM(rawData)).window;
     const jsonAttendance = [];
-    window.$("#gvList > tbody > tr")
-      .each( (index, element) => {
-        if(index >= 1){
-          jsonAttendance.push({
-            "subject" : utils.trim(window.$( element ).children("td:eq(0)").text()),
-            "time" : utils.trim(window.$( element ).children("td:eq(1)").text()),
-            "attend" : utils.trim(window.$( element ).children("td:eq(2)").text()),
-            "late" : utils.trim(window.$( element ).children("td:eq(3)").text()),
-            "absence" : utils.trim(window.$( element ).children("td:eq(4)").text()),
-            "approved" : utils.trim(window.$( element ).children("td:eq(5)").text()),
-            "menstrual" : utils.trim(window.$( element ).children("td:eq(6)").text()),
-            "early" : utils.trim(window.$( element ).children("td:eq(7)").text())
-          });
-        }
-      });
+    const elementNav = "#gvList > tbody > tr";
+    const items = document.querySelectorAll(elementNav);
+
+    for(let i=0; i<items.length; i++){
+      if(index >= 1){
+        jsonAttendance.push({
+          "subject" : utils.trim(items[i].children[0].textContent),
+          "time" : utils.trim(items[i].children[1].textContent),
+          "attend" : utils.trim(items[i].children[2].textContent),
+          "late" : utils.trim(items[i].children[3].textContent),
+          "absence" : utils.trim(items[i].children[4].textContent),
+          "approved" : utils.trim(items[i].children[5].textContent),
+          "menstrual" : utils.trim(items[i].children[6].textContent),
+          "early" : utils.trim(items[i].children[7].textContent)
+        });
+      }
+    };
 
     // JSON 으로 처리하여 클라이언트에 응답
-    res.send(JSON.stringify({
+    res.json({
       "attendance" : jsonAttendance
-    }));
+    });
 
-  }).catch((err) => { console.log(err) });
+  }).catch((err) => {
+    console.log(err)
+  });
 }
 
 module.exports = run;
