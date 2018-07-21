931afa6f681244d89792f95392a2f41a2f9369d1
Youngbin Han <sukso96100@gmail.com>
2018-07-21 23:06:48 +0900

    Revert "[hotfix/schedules] 학사일정 조회 pupeteer 로 다시 구현."

--- routes/life/schedules.js
+++ routes/life/schedules.js
@@ -1,35 +1,33 @@
 const utils = require("../utils");
 const jsdom = require("jsdom");
-const puppeteer = require("puppeteer");
+const { JSDOM } = jsdom;
 
 // 학사일정 조회
-const run = async (req, res, next) => {
+const run = (req, res, next) => {
 	console.log("POST /life/schedules");
 	const url = `${utils.skhuBaseUrl}/calendar/calendar_list_1.aspx?strYear=${req.body.year}&strMonth=${req.body.month}`;
-	try{
-		const browser = await puppeteer.launch(); // Puppeteer 초기화
-		const page = await browser.newPage(); // 페이지 생성
-		await page.setJavaScriptEnabled(true);
-		await page.goto(url);
-		const nav = "div#cont > div.coll_schedule > div.top > div.calendar > div.info > table > tbody > tr";
-		const items = await page.$$eval(nav, (nodes)=>{
-			const datas = [];
-			for(let i=1; i<nodes.length; i++){
-				datas.push({
-					"period": nodes[i].children[0].textContent,
-					"content": nodes[i].children[1].textContent
+	utils.get(req, res, url, true)
+		.then((rawData)=>{
+			const { document } = (new JSDOM(rawData)).window;
+			// 학사 일정 파싱
+			const schedules = [];
+			const table = document.querySelectorAll("div.info > table > tbody > tr");
+			for(let i=1; i<table.length; i++){
+				const item = table[i].querySelectorAll("td");
+				schedules.push({
+					"period": item[0].textContent,
+					"content": item[1].textContent
 				});
 			}
-			return datas;
+			// JSON 으로 처리하여 클라이언트에 응답
+			res.send(JSON.stringify({
+				"schedules": schedules
+			}));
+		})
+		.catch((err)=>{
+			console.log(err);
+			res.sendStatus(500);
 		});
-		await browser.close();
-		res.json({
-			"schedules": items
-		});
-	}catch(err){
-		console.error(err);
-		res.sendStatus(500);
-	}
 };
 
 module.exports = run;
