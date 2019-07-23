1ff545dff6d2b9103743164200a5d0945ef8f0e5
tpdns90321 <tpdns9032100@gmail.com>
2019-07-23 09:39:42 +0900

    [feature/healthcheck] eslint 실행

--- routes/healthcheck.js
+++ routes/healthcheck.js
@@ -2,13 +2,13 @@
 // 간단히 서버 상태를 검사하고 메세지를 출력하는 라우팅
 
 const run = async function(req, res, next){
-  // puppeteer 의 작동 여부 검사
-  const pconn = require("./puppeteer_connection");
-  var context = await pconn.getContext();
-  context.close();
+	// puppeteer 의 작동 여부 검사
+	const pconn = require("./puppeteer_connection");
+	context = await pconn.getContext();
+	context.close();
 
 	// 요청에 대한 응답으로 전송
-	res.send('OK');
+	res.send("OK");
 };
 
 // run 함수를 다른 모듈에서 사용 가능하도록 노출
